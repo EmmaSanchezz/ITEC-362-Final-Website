d89aeb86fcd127462ac941a77de6917393ee3b33
EmmaSanchezz <97483986+EmmaSanchezz@users.noreply.github.com>
2024-04-17 14:53:26 -0400

    Add files via upload

---
+++ checkoutscript.js
@@ -0,0 +1,20 @@
+
+/*document.addEventListener('DOMContentLoaded', function() {
+    const formSelector = document.getElementById('form-select'); 
+    const form1 = document.getElementById('form1');
+    const form2 = document.getElementById('form2');
+
+    function showSelectedForm() {
+        const selectedForm = formSelector.value;
+        if(selectedForm == 'form1') {
+            form1.classList.remove('hidden');
+            form2.classList.add('hidden');
+        }
+        else if(selectedForm == 'form2') {
+            form2.classList.remove('.hidden');
+            form1.classList.add('hidden');
+        }
+    }
+    formSelector.addEventListener('change', showSelectedForm);
+    showSelectedForm();
+});*/
---
+++ indexscript.js
@@ -0,0 +1,13 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelector('.slide');
+    let currentSlide = 0;
+    const totalSlides = slides.length;
+
+    function showNextSlide() {
+        slides[currentSlide].style.transform = 'translateX(-100%)';
+        currentSlide = (currentSlide + 1) % totalSlides;
+        slides[currentSlide].style.transform = 'translateX(0)';
+    }
+    setInterval(showNextSlide, 3000);
+})*/
